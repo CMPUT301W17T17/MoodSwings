962eab5be101b72c3473af7a94e9995a282f3a96
machung21 <machung@ualberta.ca>
2017-03-05 16:01:55 -0700

    added link between popup and editMood

--- app/src/main/java/com/example/android/sendmoods/EditMoodActivity.java
+++ app/src/main/java/com/example/android/sendmoods/EditMoodActivity.java
@@ -1,12 +1,20 @@
 package com.example.android.sendmoods;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 /**
  *
  */
 
-public class EditMoodActivity extends Activity{
+public class EditMoodActivity extends AppCompatActivity{
 
-    //MoodEvent mood = new MoodEvent();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.edit_mood);
+    }
+    
 }

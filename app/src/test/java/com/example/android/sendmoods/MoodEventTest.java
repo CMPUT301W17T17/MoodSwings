a2ef5165eafb4c9d7ae7860b3a64132b2857eb93
hobiehobie <ahua@ualberta.ca>
2017-03-09 16:49:04 -0700

    wrote some unit tests

---
+++ app/src/test/java/com/example/android/sendmoods/MoodEventTest.java
@@ -0,0 +1,36 @@
+package com.example.android.sendmoods;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+/**
+ * Created by ahua on 3/9/17.
+ */
+
+public class MoodEventTest extends ActivityInstrumentationTestCase2{
+
+    public MoodEventTest(){
+        super(MoodListActivity.class);
+    }
+
+    public void testgetPopupShape() {
+        MoodEvent testmood = new MoodEvent();
+        testmood.setPopupShape(0);
+        int shape = testmood.getPopupShape();
+        assertEquals(shape, 0);
+    }
+
+    public void testsetPopupShape() {
+        /*same as testgetPopupShape()?*/
+    }
+
+    public void testgetColor() {
+        MoodEvent testmood = new MoodEvent();
+        testmood.setColor("Green");
+        assertEquals(testmood.getColor(),"Green");
+    }
+
+    public void testsetColor() {
+        /*same as testgetColor()?*/
+    }
+    
+}

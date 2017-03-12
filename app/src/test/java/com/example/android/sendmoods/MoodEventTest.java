b20b11845ac3c13e40328e9f70d23698a3cabef7
hobiehobie <ahua@ualberta.ca>
2017-03-12 14:51:55 -0600

    added unit test for moodevent class

--- app/src/test/java/com/example/android/sendmoods/MoodEventTest.java
+++ app/src/test/java/com/example/android/sendmoods/MoodEventTest.java
@@ -1,6 +1,9 @@
 package com.example.android.sendmoods;
 
+import android.location.Location;
 import android.test.ActivityInstrumentationTestCase2;
+
+import java.util.Date;
 
 /**
  * Created by ahua on 3/9/17.
@@ -12,25 +15,31 @@ public class MoodEventTest extends ActivityInstrumentationTestCase2{
         super(MoodListActivity.class);
     }
 
-    public void testgetPopupShape() {
-        MoodEvent testmood = new MoodEvent();
-        testmood.setPopupShape(0);
-        int shape = testmood.getPopupShape();
-        assertEquals(shape, 0);
+    public void testMoodEventCreate(){
+        /*test on create of mood event*/
+        Location location = new Location("eventlocation");
+        location.setLatitude(53.5443d);
+        location.setLongitude(-113.4909d);
+
+        Date date = new Date();
+
+        MoodEvent moodevent = new MoodEvent(date, "bought an ice cream", "hobiehobie", location, "Happy");
+
+        Date mooddate = moodevent.getDateTime();
+        String moodreason = moodevent.getReason();
+        String mooduser = moodevent.getUsername();
+        Location moodlocation = moodevent.getAddress();
+        String moodmood = moodevent.getEmotion();
+
+        assertEquals(mooddate, date);
+        assertEquals(moodreason, "bought an ice cream");
+        assertEquals(mooduser, "hobiehobie");
+        assertEquals(moodlocation, location);
+        assertEquals(moodmood, "Happy");
     }
 
-    public void testsetPopupShape() {
-        /*same as testgetPopupShape()?*/
-    }
+    /*add testing for parcelable functionality*/
 
-    public void testgetColor() {
-        MoodEvent testmood = new MoodEvent();
-        testmood.setColor("Green");
-        assertEquals(testmood.getColor(),"Green");
-    }
 
-    public void testsetColor() {
-        /*same as testgetColor()?*/
-    }
 
 }

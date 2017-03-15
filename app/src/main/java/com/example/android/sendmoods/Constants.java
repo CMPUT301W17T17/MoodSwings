705b1cf84b2b7c862380f8242da184e872653693
mjenius <Moe@MJs-Macbook-Pro.local>
2017-03-15 15:00:11 -0600

    Constants Updated

--- app/src/main/java/com/example/android/sendmoods/Constants.java
+++ app/src/main/java/com/example/android/sendmoods/Constants.java
@@ -1,16 +1,67 @@
 package com.example.android.sendmoods;
 
-/**
- * Created by Moe on 2017-03-11.
- */
-
 public class Constants {
+
+    /**
+     * Constants for Happy Mood
+     */
     public static final int HAPPY_POPUP_BOX = R.drawable.popup_shape_pink;
     public static final String HAPPY_WORD = "Happy";
     public static final String HAPPY_COLOR = "#F06292";
 
+    /**
+     * Constants for Angry Mood
+     */
+    public static final int ANGRY_POPUP_BOX = R.drawable.popup_shape_red;
+    public static final String ANGRY_WORD = "Angry";
+    public static final String ANGRY_COLOR = "#E57373";
+
+    /**
+     * Constants for Confused Mood
+     */
+    public static final int CONFUSED_POPUP_BOX = R.drawable.popup_shape_purple;
+    public static final String CONFUSED_WORD = "Confused";
+    public static final String CONFUSED_COLOR = "#BA68C8";
+
+    /**
+     * Constants for Disgusted Mood
+     */
+    public static final int DISGUSTED_POPUP_BOX = R.drawable.popup_shape_green;
+    public static final String DSIGUSTED_WORD = "Disgusted";
+    public static final String DISGUSTED_COLOR = "#81C784";
+
+    /**
+     * Constants for fear Mood
+     */
+    public static final int AFRAID_POPUP_BOX = R.drawable.popup_shape_cyan;
+    public static final String AFRAID_WORD = "Afraid";
+    public static final String AFRAID_COLOR = "#4DD0E1";
+
+    /**
+     * Constants for sadness Mood
+     */
+    public static final int SAD_POPUP_BOX = R.drawable.popup_shape_blue;
+    public static final String SAD_WORD = "Sad";
+    public static final String SAD_COLOR = "#64B5F6";
+
+    /**
+     * Constants for surprised Mood
+     */
+    public static final int SURPRISED_POPUP_BOX = R.drawable.popup_shape_blue;
+    public static final String SURPRISED_WORD = "Surprised";
+    public static final String SURPRISED_COLOR = "#FFF176";
+
+    /**
+     * Constants for shame Mood
+     */
+    public static final int SHAME_POPUP_BOX = R.drawable.popup_shape_blue;
+    public static final String SHAME_WORD = "Ashamed";
+    public static final String SHAME_COLOR = "#FFB74D";
+
     //Changed because both constants were the same, defeating the purpose of request codes.
     //Please read up on Request codes.
+
+    //
     public static final int REQ_CODE_POPUP = 1;
     public static final int REQ_CODE_NEW = 0;
     public static final int REQ_CODE_EDIT = 2;
